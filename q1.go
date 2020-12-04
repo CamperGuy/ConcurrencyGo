d1e9db7086661161bb0a1e374b62ebd2bc054aae
CamperGuy <helblingjoel@gmail.com>
2020-12-04 10:34:34 +0000

    Implemented patient from Q2

--- q1.go
+++ q1.go
@@ -1,25 +1,5 @@
 package main
 
-/*
-Notes:
-Treatment should be done as a handshake
-
-Waiting room can fit every patient
-- Don't worry about behaviour where this is an issue
-Use message passing
-Asynchronous channel (wait) for the waiting queue
-Synchronous channel (dent) to model the state (sleeping/awake) of the DENTIST
-	! Dentist should fall asleep when reading on dent, not while reading on wait !
-Use synchronous channels (one per patient) to model the state (sleeping/awake) of that patient
-	a) patient (function) creates this as a fresh channel (chan int)
-	b) This channel will be added to the wait (chan chan int) if the patient needs to stay in
-	   the waiting room
-	c) Patient sleeps while waiting to read on their fresh channel when:
-		1) waiting in the queue
-		2) having some treatment done
-Treatment should take a random time
-*/
-
 import (
 	"fmt"
 	"math/rand"
@@ -27,90 +7,45 @@ import (
 	"time"
 )
 
-/* 	--- Task ---
-Check for people in the waiting room
-If there is a patient, call the first one in
-During treatment, dentist is active and the patient is sleeping (blocked)
-When finshed with treatment, patient is woken up
-Dentist checks for next person in the waiting room
-*/
 func dentist(wait <-chan chan int, dent <-chan chan int, comms chan<- string) {
-	busy := false
 	admissionQueue := make(chan chan int, 5)
 
-	// Direct Admission
 	go func() {
 		for {
-			patientChan := <-dent
-			if busy {
-				patientChan <- -203 // Send rejection signal. They will now request the waiting room
-				comms <- "  Dentist: Direct admission rejected Patient"
-			} else {
-				patientChan <- -202 // Send acceptance signal
-				comms <- "  Dentist: Direct admission accepted Patient"
+			select {
+			case patient := <-wait:
+				admissionQueue <- patient
+				break
+			default:
+				wakeMeUpPatient := <-dent
+				wakeMeUpPatient <- -200
 			}
-
-			admissionQueue <- patientChan
 		}
 	}()
 
-	// Waiting Room
 	go func() {
-		for {
-			patientChan := <-wait
-
-			patientChan <- -201 // Send rejection signal to send them to sleep
-			comms <- "  Dentist: Patient admitted to waiting room"
-
-			admissionQueue <- patientChan // Add them to our queue
-		}
-	}()
-
-	// Treatment
-	go func() {
-		for {
-			patientChan := <-admissionQueue
-			busy = true
-			patientChan <- -100 // Send them to sleep
+		for patient := range admissionQueue {
+			patient <- -101
 			treatmentTime := rand.Intn(6-1) + 1
-			comms <- "  Dentist: Treating a patient for " + strconv.Itoa(treatmentTime) + " seconds"
+			comms <- "Dentist: Treating a patient for " + strconv.Itoa(treatmentTime) + " seconds"
 			time.Sleep(time.Duration(treatmentTime) * time.Second)
-			patientChan <- -101
-			busy = false
+			patient <- -100
 		}
 	}()
 }
 
-/* 	--- Task ---``
-Check if the dentist is busy. If so, fall asleep (block?)
-If dentist is sleeping:
-	Wake up dentist and self fall asleep
-	- We will be woken up when the treatment is completed
-If dentist is busy with another patient:
-	Go to the waiting room
-	Sleep
-	When woken up, go to sleep after treatment started
-*/
 func patient(wait chan<- chan int, dent chan<- chan int, id int, comms chan<- string) {
 	self := make(chan int)
-	dent <- self
-	go func() {
-		for {
-			response := <-self
-			// comms <- "Patient " + strconv.Itoa(id) + ": received response " + strconv.Itoa(response)
+	dent <- self // wake up the dentist if they were asleep
+	<-self       // Accept confirmation that dentist is awake
 
-			if response == -203 {
-				// Direct admission unsuccessful
-				comms <- "Patient " + strconv.Itoa(id) + ": going to waiting room"
-				wait <- self
-			} else if response == -201 {
-				// Go to sleep to be woken up in the waiting room
-				comms <- "Patient " + strconv.Itoa(id) + ": falling asleep in the waiting room"
-			} else if response == -100 {
-				comms <- "Patient " + strconv.Itoa(id) + ": I will receive treatment"
-				if <-self == -101 {
-					comms <- "Patient " + strconv.Itoa(id) + ": I'm done!\n"
-				}
+	wait <- self // Hey, I'd like to be treated
+	go func() {
+		for sleepMessage := range self {
+			fmt.Println("Patient " + strconv.Itoa(id) + " about to be treated")
+			if sleepMessage == -101 {
+				<-self
+				fmt.Println("   Patient " + strconv.Itoa(id) + " done\n")
 			}
 		}
 	}()
@@ -131,12 +66,11 @@ func main() {
 	// channel of size n
 	go dentist(wait, dent, comms)
 	time.Sleep(3 * time.Second)
-	comms <- "\n"
+	comms <- strconv.Itoa(m) + " patients"
 	for i := 0; i < m; i++ {
 		go patient(wait, dent, i, comms)
 		time.Sleep(1 * time.Second)
-		comms <- "\n"
 	}
 	comms <- "Main: Everything initialised"
-	time.Sleep(1 * time.Second)
+	time.Sleep(30 * time.Second)
 }
